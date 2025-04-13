913dcafe348587e2eea766cf3b2afd40fb1862eb
fabenejr <fabenejunior@hotmail.com>
2025-04-13 10:32:21 -0300

    Update base path configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fabenejr.github.io/'  // Corrigindo o caminho base para o GitHub Pages
+  base: '',  // Alterando para uma string vazia já que o repositório é *.github.io
 })
