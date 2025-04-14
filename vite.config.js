2fe7351705443463050d9955e8d8d7b2f21186a0
paulo.fabene <fabenejunior@hotmail.com>
2025-04-14 14:10:33 -0300

    adjust vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/fabenejr.github.io/',
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
